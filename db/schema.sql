e7c71e0feb10de50c8fbb9d7ec2dd93aea83202e
terahje <terahjequinn@gmail.com>
2020-10-18 09:37:13 -0700

    small refactor

--- db/schema.sql
+++ db/schema.sql
@@ -10,9 +10,9 @@ CREATE TABLE department (
 CREATE TABLE roles (
   id INTEGER PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
+  salary DECIMAL(10,2),
   department_id INTEGER,
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 
@@ -21,10 +21,7 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER NOT NULL,
-  department_id INTEGER,
-  salary INTEGER,
   manager_id INTEGER,
-  CONSTRAINT fk_role FOREIGN KEY (department_id) REFERENCES roles(id) ON DELETE SET NULL,
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -12,16 +12,16 @@ VALUES
 ('Finance', '125000', 3),
 ('Legal', '190000', 4);
 
-INSERT INTO employee (first_name, last_name, role_id, department_id, salary, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-    ('John', 'Doe', 'Sales Lead', 1, 100000, 'Ashley Rodriguez'),
-    ('Mike', 'Chan', 'Salesperson', 1, 80000, 'John Doe'),
-    ('Jack E', 'Chan', 'Salesperson', 1,  75000, 'John Doe'),
-    ('Ashley Rodriguez', 'Doe', 'Lead Engineer', 2, 150000, NULL),
-    ('Kevin', 'Tupik', 'Software Engineer', 2, 120000, 'Ashley Rodriquez'),
-    ('Keith', 'Turner', 'Software Engineer', 2, 120000, 'Ashley Rodriquez'),
-    ('Mario', 'Miller', 'Software Engineer', 2, 100000, 'Ashley Rodriquez'),
-    ('Malia', 'Brown', 'Accountant', 3, 125000, NULL),
-    ('Sarah', 'Lourd', 'Legal Team Lead', 4, 250000, NULL),
-    ('Ted', 'Gallon', 'Lawyer', 4,  190000, 'Sarah Lourd');
+    ('John', 'Doe', 'Sales Lead', 'Ashley Rodriguez'),
+    ('Mike', 'Chan', 'Salesperson', 'John Doe'),
+    ('Jack E', 'Chan', 'Salesperson', 'John Doe'),
+    ('Ashley Rodriguez', 'Doe', 'Lead Engineer', NULL),
+    ('Kevin', 'Tupik', 'Software Engineer', 'Ashley Rodriquez'),
+    ('Keith', 'Turner', 'Software Engineer', 'Ashley Rodriquez'),
+    ('Mario', 'Miller', 'Software Engineer','Ashley Rodriquez'),
+    ('Malia', 'Brown', 'Accountant', NULL),
+    ('Sarah', 'Lourd', 'Legal Team Lead', NULL),
+    ('Ted', 'Gallon', 'Lawyer', 'Sarah Lourd');
    
